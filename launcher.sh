46515ae647fcd7321868c2205fba366582433c3f
Lorenzo Ricciardi <lorenzo.jones112@gmail.com>
2025-10-14 22:41:47 +0200

    added 1-to-1 nat

--- launcher.sh
+++ launcher.sh
@@ -3,33 +3,51 @@
 # Each step is printed for better debugging and tracking
 
 # --- STEP 1: Add Tayga route if it doesn't exist ---
-ROUTE="172.17.0.128/25"
 GATEWAY="172.17.0.2"
+ROUTE="193.110.0.0/24"
 IFACE="docker0"
 
 # Add Tayga route to host
 if ! ip route show | grep -q "$ROUTE"; then
-    echo "Route $ROUTE not found. Adding route via $GATEWAY dev $IFACE"
+    echo "Route $ROUTE not found. Adding $ROUTE via $GATEWAY dev $IFACE"
     sudo ip route add $ROUTE via $GATEWAY dev $IFACE
     echo "✅ Route added."
 else
-    echo "✅ Route $ROUTE already exists."
+    echo "✅ Route $ROUTE via $GATEWAY dev $IFACE already exists."
 fi
 
 # Update /etc/hosts with the specified domains ---
 HOSTS_FILE="/etc/hosts"
 IPV4="$GATEWAY"
 
-DOMAINS=("helpdesk.theboys.it" "manager.theboys.it" "roundcube.theboys.it" "sysloghub.theboys.it"    # wsa1's virtual host
-         "ecopulse.theboys.it" "invest.theboys.it" "quix.theboys.it" "techsolve.theboys.it"   # wsa2's virtual host
-         "agency.theboys.it" "blackhoney.theboys.it" "myrecipe.theboys.it")                   # wsn's virtual host
+DOMAINS=("helpdesk.theboys.it" "mail.theboys.it" "manager.theboys.it" "sysloghub.theboys.it"   # wsa1's virtual host
+         "ecopulse.theboys.it" "invest.theboys.it" "quix.theboys.it" "techsolve.theboys.it"    # wsa2's virtual host
+         "agency.theboys.it" "blackhoney.theboys.it" "myrecipe.theboys.it" "puffcats.theboys.it")                    # wsn's virtual host
 
-echo "✅ Updating /etc/hosts"
-for domain in "${DOMAINS[@]}"; do
-    if ! grep -q "$domain" "$HOSTS_FILE"; then
-        echo "$IPV4 $domain" >> "$HOSTS_FILE"
-    fi
-done
+echo "✅ Updated /etc/hosts with ip <-> domain"
+if ! grep -q  "Beeware hosts" "$HOSTS_FILE"; then
+    cat <<EOF >> "$HOSTS_FILE"
+####################################
+#           Beeware hosts          #
+####################################
+# wsa2
+193.110.0.22   ecopulse.theboys.it
+193.110.0.22   invest.theboys.it
+193.110.0.22   quix.theboys.it
+193.110.0.22   techsolve.theboys.it
+# wsn
+193.110.0.23   agency.theboys.it
+193.110.0.23   blackhoney.theboys.it
+193.110.0.23   myrecipe.theboys.it
+193.110.0.23   puffcats.theboys.it
+EOF
+fi
+
+# for domain in "${DOMAINS[@]}"; do
+#     if ! grep -q "$domain" "$HOSTS_FILE"; then
+#         echo "$IPV4 $domain" >> "$HOSTS_FILE"
+#     fi
+# done
 
 # Clean the Kathara lab ---
 kathara wipe -f && echo "✅ Kathara lab cleaned."
--- machines_startup_script/backbone/nat64.sh
+++ machines_startup_script/backbone/nat64.sh
@@ -1,9 +1,11 @@
 
 #!/bin/bash
 
-TAYGA_CONF_IPV4_ADDR="172.17.0.2"
+# TAYGA_CONF_IPV4_ADDR="172.17.0.2"
+TAYGA_CONF_IPV4_ADDR="193.110.0.1"
 TAYGA_CONF_PREFIX="2001:db8:64:ff9b::/96"
-TAYGA_CONF_DYNAMIC_POOL="172.17.0.128/25"
+# TAYGA_CONF_DYNAMIC_POOL="172.17.0.128/25"
+TAYGA_CONF_DYNAMIC_POOL="193.110.0.0/24"
 TAYGA_CONF_DATA_DIR="/var/db/tayga"
 TAYGA_CONF_DIR="/etc"
 
@@ -20,11 +22,27 @@ ipv4-addr ${TAYGA_CONF_IPV4_ADDR}
 prefix ${TAYGA_CONF_PREFIX}
 dynamic-pool ${TAYGA_CONF_DYNAMIC_POOL}
 data-dir ${TAYGA_CONF_DATA_DIR}
+
+# map ipv4 to ipv6
+# Lan B
+map 193.110.0.12    2a04:0:0:1::2   # wsa1
+map 193.110.0.13    2a04:0:0:1::3   # mdb
+map 193.110.0.14    2a04:0:0:1::4   # smb
+map 193.110.0.15    2a04:0:0:1::5   # mxs
+# Lan C
+map 193.110.0.22    2a04:0:0:2::2   # wsa2
+map 193.110.0.23    2a04:0:0:2::3   # wsn
+map 193.110.0.24    2a04:0:0:2::4   # ovpn
+map 193.110.0.25    2a04:0:0:2::5   # bind2
 EOF
 
 # Setup Tayga networking
 tayga -c ${TAYGA_CONF_DIR}/tayga.conf --mktun
 ip link set nat64 up
+
+# echo "172.17.0.202    2a04::2    1760189567" >> /var/db/tayga/dynamic.map
+# echo "172.17.0.203    2a04::3    1760189567" >> /var/db/tayga/dynamic.map
+# echo "172.17.0.204    2a04::4    1760189567" >> /var/db/tayga/dynamic.map
 
 ip route add ${TAYGA_CONF_DYNAMIC_POOL} dev nat64
 ip route add ${TAYGA_CONF_PREFIX} dev nat64
