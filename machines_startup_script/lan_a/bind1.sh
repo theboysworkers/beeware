0a0af96ce5ff6ba487b0763f74fe4b43e104c9d3
Lorenzo Ricciardi <lorenzo.jones112@gmail.com>
2025-11-03 16:07:02 +0100

    fixed startup

--- machines_startup_script/lan_a/bind1.sh
+++ machines_startup_script/lan_a/bind1.sh
@@ -21,7 +21,7 @@ rsyslogd -N1 -f /etc/rsyslog.d/20-forward-logs.conf
 # Start the rsyslog service
 systemctl start rsyslog
 
-# Append the rule that defines ListenAddress only on eth0
+# Append the rule that defines ListenAddress only on eth0 (VLAN managed M1)
 echo "ListenAddress fe80::200:ff:fe00:a3%eth0" >> /etc/ssh/sshd_config
 
 # Start the SSH service
